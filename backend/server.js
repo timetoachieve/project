1d01557a877be4505aa1694f18fca99d07a4d9cf
timetoachieve <86340551+timetoachieve@users.noreply.github.com>
2021-08-01 17:53:32 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import data from "./data.js";
+
+require("./db/conn");
+const app = express();
+
+app.get("/api/companies", (req, res) => {
+  res.send(data.companyList);
+});
+
+app.get("/api/schedules", (req, res) => {
+  res.send(data.scheduleList);
+});
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
